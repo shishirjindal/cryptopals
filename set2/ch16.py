cf5baba97519613e8abce289b80b6a003f3ed795
Shishir Jindal <honey.jindal5@gmail.com>
2017-03-30 01:11:36 +0530

    updated set2 ch16.py

--- set2/ch16.py
+++ set2/ch16.py
@@ -1,11 +1,10 @@
 import os
-import sys
 from Crypto.Cipher import *
 
 key = os.urandom(16)
 IV = os.urandom(16)
 prepend = "comment1=cooking MCs;userdata="
-append = ";comment2=like a pound of bacon"
+append = ";comment2= like a pound of bacon"
 
 def pkcs7(plaintext):
 	number = 16-len(plaintext)%16
@@ -24,7 +23,8 @@ def AES_CBC_decrypt(ciphertext):
 	return aes.decrypt(ciphertext)
 
 def function1():
-	userinput = sys.argv[1]
+	print "Enter input :", 
+	userinput = raw_input()
 	userinput = userinput.replace(';','').replace('=','')
 	plaintext = prepend+userinput+append
 	plaintext = pkcs7(plaintext)
@@ -36,9 +36,9 @@ def function2(ciphertext):
 	plaintext = unpad(plaintext)
 	plaintext = plaintext.split(";")
 	if "admin=true" in plaintext:
-		return True
+		print "Welcome Admin!!"
 	else:
-		return False
+		print "Get the hell out of here"
 
 
 ciphertext = function1()
@@ -47,5 +47,4 @@ ciphertext = [ciphertext[i:i+16] for i in range(0,len(ciphertext),16)]
 # now i change the ciphertext such that after decrypting it contains admin=true for input of he$admin$true
 
 ciphertext[1] = chr(ord('$')^ord(';')^ord(ciphertext[1][0]))+ciphertext[1][1:6]+chr(ord(ciphertext[1][6])^ord('$')^ord('='))+ciphertext[1][7:]
-print function2(''.join(ciphertext))
-
+function2(''.join(ciphertext))
